87d12c8ef4d1c0442f6beecb9207fa70cf3047ad
hyperV <testersb4@gmail.com>
2025-02-28 07:57:54 +0100

    add def

--- rr.py
+++ rr.py
@@ -1,6 +1,16 @@
+import time
+
+
 print("hello")
 print('dd')
 print('ds asdasd')
 
 print('sddssdsd')
+def f_print(aa):
+    for i in aa:
+        print(i)
+    print("\n")
+
+
+f_print(aa="nazwa")
 
